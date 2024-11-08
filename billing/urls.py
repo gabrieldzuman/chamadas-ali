fe5b138d2ebc2b6d33770d4d47c46a8d2abde2ac
Gabriel Dzuman <83187468+gabrieldzuman@users.noreply.github.com>
2024-11-07 23:19:02 -0300

    Update urls.py

--- billing/urls.py
+++ billing/urls.py
@@ -1,10 +1,7 @@
+from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import CallRecordViewSet
-
-router = DefaultRouter()
-router.register(r'call-records', CallRecordViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('admin/', admin.site.urls),
+    path('api/', include('billing.urls')),  
 ]
